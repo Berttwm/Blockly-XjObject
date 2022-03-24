d74c774c42ba2355a0d0b8b78733bed817a97094
Bertrand Tan <bertrandTWM@gmail.com>
2022-03-24 18:09:13 +0800

    [Bugfix] Fixed static file

--- code.js
+++ code.js
@@ -495,7 +495,7 @@ Code.init = function() {
            length: 3,
            colour: '#ccc',
            snap: true},
-       media: '../../media/',
+       media: './media/',
        rtl: rtl,
        toolbox: toolboxXml,
        zoom:
@@ -658,8 +658,8 @@ Code.discard = function() {
 };
 
 // Load the Code demo's language strings.
-document.write('<script src="msg/' + Code.LANG + '.js"></script>\n');
+document.write('<script src="./msg/' + Code.LANG + '.js"></script>\n');
 // Load Blockly's language strings.
-document.write('<script src="../../msg/js/' + Code.LANG + '.js"></script>\n');
+document.write('<script src="./msg/js/' + Code.LANG + '.js"></script>\n');
 
 window.addEventListener('load', Code.init);
